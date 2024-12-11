398f499ab2c5e0e715a58f441c99bc0942d15bdd
“Dmytro <Kulish.dmytro@gmail.com>
2024-12-11 11:48:08 +0200

    2nd page and tests

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page_view(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Homepage") 
+
+def about_page_view(request):  
+    context = {"name": "Dmytro Kulish", 
+               'age': 47}
+    
+    return render(request, "pages/about.html", context) 
